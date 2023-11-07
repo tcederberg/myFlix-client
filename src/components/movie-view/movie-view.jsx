4512c7f469f1720f7d44010f2f9cf9f0ffe65098
tcederberg <trcederberg@gmail.com>
2023-11-07 08:10:07 -0500

    updated movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -39,13 +39,9 @@ MovieView.propTypes = {
         Description: PropTypes.string.isRequired,
         Genre: PropTypes.shape({
             Name: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired
         }),
         Director: PropTypes.shape({
             Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-            Birth: PropTypes.string.isRequired,
-            Death: PropTypes.string
         }),
         Featured: PropTypes.bool.isRequired
     }).isRequired,
