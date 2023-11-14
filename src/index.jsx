e64279fa75d5540251d10c3b03601c26ee17d261
tcederberg <trcederberg@gmail.com>
2023-11-14 15:09:10 -0500

    index.jsx update

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
 import { Container } from "react-bootstrap";
-import { store } from "./redux/store";
+import { store } from "../redux/store";
 import { Provider } from "react-redux";
 
 import "./index.scss";
